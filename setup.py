cd639346bd8dcd97b9e110d1e7ff0411ca835d3e
joesun16 <joesun16@users.noreply.github.com>
2025-12-20 14:45:06 +0800

    V2.3.4: Switch to py2app for macOS build - fixes ARM crash

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+APP = ['video_compressor.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': False,
+    'iconfile': 'icon.icns',
+    'plist': {
+        'CFBundleName': 'SheCan视频压缩工具',
+        'CFBundleDisplayName': 'SheCan视频压缩工具',
+        'CFBundleIdentifier': 'com.shecan.videocompressor',
+        'CFBundleVersion': '2.3.3',
+        'CFBundleShortVersionString': '2.3.3',
+        'NSHighResolutionCapable': True,
+    },
+    'packages': ['PyQt6'],
+    'includes': ['PyQt6.QtWidgets', 'PyQt6.QtCore', 'PyQt6.QtGui'],
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
